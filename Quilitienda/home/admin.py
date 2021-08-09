65f4a477fd72d04fce0e4e767b035432bb2b305d
Jamilson <japonza559@misena.edu.co>
2021-08-09 01:28:58 -0500

    Creacion de super usuario y primera migracion

--- Quilitienda/home/admin.py
+++ Quilitienda/home/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from .models import Comprador, Categoria, Producto, Administrador, Pedido, Envio
 
-# Register your models here.
+admin.site.register(Comprador)
+admin.site.register(Categoria)
+admin.site.register(Producto)
+admin.site.register(Administrador)
+admin.site.register(Pedido)
+admin.site.register(Envio)
